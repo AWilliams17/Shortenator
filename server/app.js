78b606175700001780be82951829f498265a9f82
AWilliams17 <awilliams17411@gmail.com>
2019-05-27 22:59:23 -0500

    Added dotenv, .env.template, and babel

--- server/app.js
+++ server/app.js
@@ -14,7 +14,6 @@ app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
-
 // set up api
 let apiRoutes = require('./api/api-routes');
 app.use('/api', apiRoutes);
