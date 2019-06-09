d3a90c17b1492159af8c2522b10f9b2b05f4089d
AWilliams17 <awilliams17411@gmail.com>
2019-06-09 00:50:42 -0500

    Client: Index.html, root.js

---
+++ client/src/components/root.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Index extends Component {
+    render() {
+        return (
+            <React.Fragment>
+                <p>Root/Index Component</p>
+            </React.Fragment>
+        );
+    }
+}
+
+export default Index;
